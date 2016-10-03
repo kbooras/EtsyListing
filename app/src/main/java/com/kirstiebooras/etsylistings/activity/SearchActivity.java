348108f69471a0935d07dfbb2e43a16382d7c265
kbooras <kbooras@ufl.edu>
2016-10-02 17:46:58 -0700

    Add cancel button to search edit text

--- app/src/main/java/com/kirstiebooras/etsylistings/activity/SearchActivity.java
+++ app/src/main/java/com/kirstiebooras/etsylistings/activity/SearchActivity.java
@@ -1,14 +1,18 @@
 package com.kirstiebooras.etsylistings.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -34,6 +38,9 @@ public class SearchActivity extends AppCompatActivity {
     private ResultAdapter mResultAdapter;
     private String mCurrentSearchKey;
 
+    private EditText mSearchEditText;
+    private TextWatcher mTextChangedListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,21 +53,22 @@ public class SearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
 
-        final EditText searchBar = (EditText) findViewById(R.id.search_bar);
-        searchBar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        mSearchEditText = (EditText) findViewById(R.id.search_edit_text);
+        mTextChangedListener = new TextWatcher() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    if (mResults != null) {
-                        mResults.clear();
-                    }
-                    mCurrentSearchKey = searchBar.getText().toString();
-                    performSearch(mCurrentSearchKey, STARTING_OFFSET);
-                    return true;
-                }
-                return false;
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
-        });
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                showSearchCancelButton(mSearchEditText, editable.length() > 0);
+            }
+        };
+        setupSearchEditTextListeners();
 
         GridLayoutManager mLayoutManager = new GridLayoutManager(this, NUM_COLUMNS);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.results_view);
@@ -72,6 +80,68 @@ public class SearchActivity extends AppCompatActivity {
                 performSearch(mCurrentSearchKey, currentOffset);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSearchEditText.addTextChangedListener(mTextChangedListener);
+    }
+
+    private void setupSearchEditTextListeners() {
+        mSearchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    if (mResults != null) {
+                        mResults.clear();
+                    }
+
+                    mSearchEditText.clearFocus();
+                    InputMethodManager inputManager =
+                            (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(mSearchEditText.getWindowToken(), 0);
+
+                    mCurrentSearchKey = mSearchEditText.getText().toString();
+                    performSearch(mCurrentSearchKey, STARTING_OFFSET);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mSearchEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                showSearchCancelButton(mSearchEditText,
+                        hasFocus && mSearchEditText.getText().length() > 0);
+            }
+        });
+
+        mSearchEditText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+
+                if(event.getAction() == MotionEvent.ACTION_UP &&
+                        event.getX() <= mSearchEditText
+                                .getCompoundDrawables()[DRAWABLE_LEFT].getBounds().width()) {
+                    mSearchEditText.setText("");
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void showSearchCancelButton(EditText searchEditText, boolean show) {
+        int drawableLeft;
+        if (show) {
+            drawableLeft = R.drawable.ic_cancel;
+        } else {
+            drawableLeft = R.drawable.ic_magnifying_glass;
+        }
+        searchEditText.setCompoundDrawablesWithIntrinsicBounds(drawableLeft, 0, 0, 0);
     }
 
     private void performSearch(String searchKey, int offset) {
